c2a866a8a7f8908f7177db76b618a47a7ea02bf0
Michael <mickeymurage@gmail.com>
2022-09-05 04:44:02 +0300

    tried postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ailments", force: :cascade do |t|
     t.string "ailment_name"
     t.string "ailment_type"
