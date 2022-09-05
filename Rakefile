990027a51ffad5641604a064066c67d175f88be4
Michael <mickeymurage@gmail.com>
2022-09-05 05:49:18 +0300

    back to default

--- Rakefile
+++ Rakefile
@@ -10,8 +10,24 @@ task :console do
   Pry.start
 end
 
+# desc "Start the server"
+# task :server do
+#   #enables one to run rake server to start server
+#   exec "bundle exec rerun -b 'rackup config.ru'"
+# end
+
 desc "Start the server"
-task :server do
-  #enables one to run rake server to start server
-  exec "bundle exec rerun -b 'rackup config.ru'"
+task :server do  
+  if ActiveRecord::Base.connection.migration_context.needs_migration?
+    puts "Migrations are pending. Make sure to run `rake db:migrate` first."
+    return
+  end
+
+  # rackup -p PORT will run on the port specified (9292 by default)
+  ENV["PORT"] ||= "9292"
+  rackup = "rackup -p #{ENV['PORT']}"
+
+  # rerun allows auto-reloading of server when files are updated
+  # -b runs in the background (include it or binding.pry won't work)
+  exec "bundle exec rerun -b '#{rackup}'"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "ailments", force: :cascade do |t|
-    t.string "ailment_name"
-    t.string "ailment_type"
-  end
-
-  create_table "doctors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone"
-    t.string "address"
-    t.string "department"
-    t.string "practice_type"
-    t.string "town"
-    t.string "ailment_type"
-    t.integer "ailment_id"
-  end
-
-  create_table "patients", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "gender"
-    t.string "birthdate"
-    t.string "email"
-    t.string "address"
-    t.string "phone"
-    t.string "town"
-    t.string "ailment_name"
-    t.string "ailment_type"
-    t.integer "ailment_id"
-  end
 
 end
